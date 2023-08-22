9dae8f713bd9aa4e7aeb36200eda843c9ce1d1db
abdulrahimalesa <abdulrahimalesa@gmali.com>
2023-08-22 14:57:38 +0300

    upDate

--- controllers/coursController.js
+++ controllers/coursController.js
@@ -29,3 +29,19 @@ exports.getAllCourses = async (req, res) => {
     });
   }
 };
+
+exports.getCourse = async (req, res) => {
+  try {
+    const course = await Cours.findOne({slug: req.params.slug});
+
+    res.status(200).render('course',{
+      course,
+      page_name: 'courses',
+    });
+  } catch (error) { // Add the 'error' parameter here
+    res.status(400).json({
+      status: 'fail',
+      error, // Now 'error' is defined and contains the actual error object
+    });
+  }
+};
