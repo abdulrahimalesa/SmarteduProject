4a42a9c091d7f0aad84243c48a320f5deb001c6b
abdulrahimalesa <abdulrahimalesa@gmali.com>
2023-08-23 11:36:01 +0300

    login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt')
 
 exports.createUser = async (req, res) => {
     try {
@@ -14,3 +15,54 @@ exports.createUser = async (req, res) => {
       });
     }
   };
+
+  // exports.loginUser = async (req, res) => {
+  //   try {
+  //     const {email, password} = req.body;
+
+  //     await User.findOne({email}, (err, user) => {
+  //       if (user){
+  //         bcrypt.compare(password, user.password, (err, same) => {
+  //           if (same){
+  //             res.status(200).send('You Are Login In');
+  //           }
+  //         })
+  //       }
+  //     })
+      
+  //   } catch (error) { // Add the 'error' parameter here
+  //     res.status(400).json({
+  //       status: 'fail',
+  //       error, // Now 'error' is defined and contains the actual error object
+  //     });
+  //   }
+  // };
+  
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      // Kullanıcı bulunamazsa bir yanıt döndürün
+      return res.status(401).json({ mesaj: 'Kullanıcı bulunamadı' });
+    }
+
+    // bcrypt.compare'ı async/await bağlamında kullanın
+    const parolaEslesme = await bcrypt.compare(password, user.password);
+
+    if (parolaEslesme) {
+      res.status(200).json({ mesaj: 'Giriş yapıldı' });
+    } else {
+      res.status(401).json({ mesaj: 'Yanlış şifre' });
+    }
+  } catch (hata) {
+    // Oluşan hataları işleyin
+    res.status(500).json({
+      durum: 'hata',
+      mesaj: 'Bir hata oluştu',
+      hata: hata.message, // Anlamlı bir hata mesajı sağlayın
+    });
+  }
+};
--- controllers/pageController.js
+++ controllers/pageController.js
@@ -8,10 +8,16 @@ exports.getAboutPage =  (req, res) => {
     res.status(200).render('about',{
         page_name: 'about'
     });
-  }
+  };
 
   exports.getRegisterPage =  (req, res) => {
     res.status(200).render('register',{
         page_name: 'register'
     });
-  }
+  };
+
+  exports.getLoginPage =  (req, res) => {
+    res.status(200).render('login',{
+        page_name: 'login'
+    });
+  };
