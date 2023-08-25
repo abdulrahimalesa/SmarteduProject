03974533ec9dc3e321b79f110ce0fd16fd458d9d
abdulrahimalesa <abdulrahimalesa@gmali.com>
2023-08-25 18:02:46 +0300

    email_eklemek

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+const nodemailer = require("nodemailer");
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
     res.status(200).render('index', {
@@ -22,3 +24,52 @@ exports.getAboutPage =  (req, res) => {
         page_name: 'login'
     });
   };
+
+  exports.getContactPage =  (req, res) => {
+    res.status(200).render('contact',{
+        page_name: 'contact'
+    });
+  };
+
+  exports.sendEmail =  async (req, res) => {
+    const outputMessage = `
+    
+    <h1>Mail Details </h1>
+    <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Email: ${req.body.email}</li>
+    </ul>
+    <h1>Message</h1>
+    <p>${req.body.message}</p>
+
+    `
+
+
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+        user: 'abdulrahimalesa507@gmail.com', // gmail account
+        pass: 'hnrisiuqowmafokc' // gmail password  
+
+      }
+    });
+    
+    // async..await is not allowed in global scope, must use a wrapper
+      // send mail with defined transport object
+      let info = await transporter.sendMail({
+        from: '"Deneme projeden gonderiyorum " <abdulrahimalesa507@gmail.com>', // sender address
+        to: "abdulrahimalesa@gmail.com", // list of receivers
+        subject: "Hello Deneme New Message", // Subject line
+        html: outputMessage, // html body
+      });
+    
+      console.log("Message sent: %s", info.messageId);
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    res.status(200).redirect('contact')
+
+  };
+  
